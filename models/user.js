d6984b1dfe5fe1375843ea59e993de36871bd4f8
Aziz Ahmed <azizahmed10088@gmail.com>
2026-07-13 22:46:07 +0500

    ui

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import "./role";
 
 const userSchema = new mongoose.Schema({
     email:       { type: String, required: true, unique: true, lowercase: true, trim: true },
