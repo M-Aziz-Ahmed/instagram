0a433689669dc9efc3a5eff3ccbfcad62d637d66
Aziz Ahmed <azizahmed10088@gmail.com>
2026-07-14 11:59:55 +0500

    ui

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import LayoutWrapper from "@/components/Layout/LayoutWrapper";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} h-full`} suppressHydrationWarning>
       <body className="h-full bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100 antialiased font-sans transition-colors duration-200">
+        <Analytics/>
         <Providers>
           <LayoutWrapper>
             {children}
