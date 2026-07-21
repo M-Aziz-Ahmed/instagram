b72d7c78d7a1dd7fbd2bfaa8286714a87c44af40
Aziz Ahmed <azizahmed10088@gmail.com>
2026-07-22 02:15:04 +0500

    ui

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,26 @@ const nextConfig = {
                 protocol: "https",
                 hostname: "res.cloudinary.com",
             },
+            {
+                protocol: "https",
+                hostname: "static.tvmaze.com",
+            },
+            {
+                protocol: "https",
+                hostname: "img.anili.st",
+            },
+            {
+                protocol: "https",
+                hostname: "cdn.anipixcdn.co",
+            },
+            {
+                protocol: "https",
+                hostname: "uploads.mangadex.org",
+            },
+            {
+                protocol: "https",
+                hostname: "cdn.animepixcdn.co",
+            },
         ],
     },
   allowedDevOrigins: ['39.62.217.128','0.0.0.0','dad-phrases-removable-car.trycloudflare.com'],
