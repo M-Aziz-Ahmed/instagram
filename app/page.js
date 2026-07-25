9bcd3b259763c95595fc55e2b7e6d565248cb94e
Aziz Ahmed <azizahmed10088@gmail.com>
2026-07-25 05:09:14 +0500

    ui

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import FeedNoSSR from "@/components/Feed/FeedNoSSR";
-
 export const metadata = {
   title: 'Home Feed',
   description: 'Discover anonymous posts, stories, and content from users around the world. Share your thoughts freely without revealing your identity.',
@@ -10,5 +8,5 @@ export const metadata = {
 };
 
 export default function Home() {
-    return <FeedNoSSR />;
+    return <Feed />;
 }
